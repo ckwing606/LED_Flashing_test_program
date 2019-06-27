536c29648e482290e77e2b6ce72b8d0d2e3dcdb1
ckwing606 <39762262+ckwing606@users.noreply.github.com>
2019-06-27 18:45:45 +0800

    Update loopin.cpp

--- loopin.cpp
+++ loopin.cpp
@@ -2,7 +2,6 @@
   Program name: WS2812 ZigZag
   Author:       Robin
   Date:         2019-06-19
-
 */
 
 //Include the library and config
@@ -16,8 +15,6 @@ const uint8_t pixelPin[] = {8, 9, 10, 11, 12, 13};
 
 //Define how many pixels for each strip
 const uint8_t pixelNum[] = {52, 52, 52, 52, 52, 52};
-
-//const uint8_t lastArr = pixelNum[pixelNum.length - 1];
 
 //Define the total number of LED Strip
 #define stripLength  6
@@ -35,12 +32,11 @@ const uint8_t pixelColor[3] = {0, 80, 255};
 const uint8_t resetColor[3] = {0, 0, 0};
 
 //Set delay time for each pixel
-int delayValue = 10;
+int delayValue = 100;
 
 //Set the first and the last pixel number of the pixel strip
 int firstPixel = 11;
-int lastPixel = 16;
-
+int lastPixel = 18;
 
 //For looper() function
 uint8_t firstStrip = 0;
@@ -55,7 +51,7 @@ int  lastPixel_o = lastPixel;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  //Serial.begin(9600);
   setupPixel();
 }
 
@@ -66,8 +62,8 @@ void loop() {
   //tester(pixelColor, resetColor);
   looper(pixelColor, resetColor);
 
-  Serial.println("----------Loop finished!----------");
-  //delayValue = 500;
+//  Serial.println("----------Loop finished!----------");
+//  delayValue = 500;
   firstStrip += 1;
   lastStrip -= 1;
   firstPixel += 1;
@@ -97,16 +93,21 @@ void setupPixel() {
 void looper(uint8_t color[3], uint8_t reset[3]) {
 
   for (int i = firstPixel; i < lastPixel; i++) {
+    Serial.print("=========");
+    Serial.println(i);
     strip[firstStrip].setPixelColor(i, strip[i].Color(color[0], color[1], color[2]));
     strip[firstStrip].show();
     delay(delayValue);
     strip[firstStrip].setPixelColor(i, strip[i].Color(reset[0], reset[1], reset[2]));
     strip[firstStrip].show();
-    Serial.print("firstStrip: ");
-    Serial.print(firstStrip);
-    Serial.print(",");
-    Serial.println(i);
+//    Serial.print("firstStrip: ");
+//    Serial.print(firstStrip);
+//    Serial.print(",");
+//    Serial.println(i);
   }
+
+
+  
 
   for (int i = firstStrip; i < lastStrip ; i++) {
     strip[i].setPixelColor(lastPixel, strip[i].Color(color[0], color[1], color[2]));
@@ -114,10 +115,10 @@ void looper(uint8_t color[3], uint8_t reset[3]) {
     delay(delayValue);
     strip[i].setPixelColor(lastPixel, strip[i].Color(reset[0], reset[1], reset[2]));
     strip[i].show();
-    Serial.print("from left to right: ");
-    Serial.print(lastPixel);
-    Serial.print(",");
-    Serial.println(i);
+//    Serial.print("from left to right: ");
+//    Serial.print(lastPixel);
+//    Serial.print(",");
+//    Serial.println(i);
   }
 
   for ( int i = lastPixel; i > firstPixel; i--) {
@@ -126,10 +127,10 @@ void looper(uint8_t color[3], uint8_t reset[3]) {
     delay(delayValue);
     strip[lastStrip].setPixelColor(i, strip[i].Color(reset[0], reset[1], reset[2]));
     strip[lastStrip].show();
-    Serial.print("lastStrip: ");
-    Serial.print(lastStrip);
-    Serial.print(",");
-    Serial.println(i);
+//    Serial.print("lastStrip: ");
+//    Serial.print(lastStrip);
+//    Serial.print(",");
+//    Serial.println(i);
   }
 
   for (int i = lastStrip ; i > firstStrip; i--) {
@@ -138,16 +139,22 @@ void looper(uint8_t color[3], uint8_t reset[3]) {
     delay(delayValue);
     strip[i].setPixelColor(firstPixel, strip[i].Color(reset[0], reset[1], reset[2]));
     strip[i].show();
-    Serial.print("from right to left: ");
-    Serial.print(firstPixel);
-    Serial.print(",");
-    Serial.println(i);
+//    Serial.print("from right to left: ");
+//    Serial.print(firstPixel);
+//    Serial.print(",");
+//    Serial.println(i);
   }
 
 }
 
 void tester(uint8_t color[3], uint8_t reset[3]) {
 
-  
+  for ( int i = 0; i < 57; i++ ){
+    strip[0].setPixelColor(i, strip[i].Color(color[0], color[1], color[2]));
+    strip[0].show();
+    delay(delayValue);
+    strip[0].setPixelColor(i, strip[i].Color(reset[0], reset[1], reset[2]));
+    strip[0].show();
+  }
 
 }
